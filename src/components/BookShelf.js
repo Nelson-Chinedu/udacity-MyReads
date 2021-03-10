c7670d023eefc49984d69055740c996591b68343
Nelson Chinedu <egwuchinedu69@gmail.com>
2021-03-10 17:48:16 +0100

    fix search/book shuffling

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import Book from './Book';
-
+import Book from "./Book";
 
 class BookShelf extends Component {
-
   static propTypes = {
     bookShelfTitle: PropTypes.string.isRequired,
-    bookList: PropTypes.array.isRequired,
-    onChangeShelf: PropTypes.func.isRequired
-  }
+    books: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired,
+  };
 
-  render(){
-    const { bookShelfTitle, bookList, onChangeShelf } = this.props;
-    
+  updateBook = (book, shelf) => {
+    this.props.onChangeShelf(book, shelf);
+  };
+
+  render() {
     return (
       <div>
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{bookShelfTitle}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {bookList.map((book, index) => {
-              return (
-                <li key={index}>
-                  <Book book={book} onChangeShelf={onChangeShelf} />
-                </li>
-              )
-            })}
-          </ol>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{this.props.bookShelfTitle}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.books.map((book, index) => (
+                <Book
+                  book={book}
+                  key={index}
+                  onUpdate={(shelf) => {
+                    this.updateBook(book, shelf);
+                  }}
+                />
+              ))}
+            </ol>
+          </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 }
-
 
 export default BookShelf;
--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import * as BooksAPI from '../BooksAPI';
-import Book from './Book';
-
+import * as BooksAPI from "../BooksAPI";
+import Book from "./Book";
 
 class SearchBook extends Component {
   static propTypes = {
@@ -12,50 +11,87 @@ class SearchBook extends Component {
     books: PropTypes.array.isRequired
   }
   state = {
-    searchResults: []
-  }
+    books: [],
+    query: "",
+  };
 
-  onSearch = (e) => {
-    const query = e.target.value;
-    if (!query) {
-        this.setState({searchResults: []});
-        return;
-    }
-    BooksAPI.search(query, 20).then(searchResults => {
-        if (searchResults.error) {
-            searchResults = [];
-        }
-        searchResults = searchResults.map((book) => {
-            const bookInShelf = this.props.books.find(b => b.id === book.id);
-            if (bookInShelf) {
-                book.shelf = bookInShelf.shelf;
-            }
-            return book;
-        });
-        this.setState({searchResults});
+  handleChange = (event) => {
+    var value = event.target.value;
+    this.setState(() => {
+      return { query: value };
     });
-};
+    this.searchBooks(value);
+  };
+
+  changeBookShelf = (books) => {
+    let allBooks = this.props.books;
+    for (let book of books) {
+      book.shelf = "none";
+    }
+
+    for (let book of books) {
+      for (let b of allBooks) {
+        if (b.id === book.id) {
+          book.shelf = b.shelf;
+        }
+      }
+    }
+    return books;
+  };
+
+  searchBooks = (value) => {
+    if (value.length !== 0) {
+      BooksAPI.search(value, 10).then((books) => {
+        if (books.length > 0) {
+          books = books.filter((book) => book.imageLinks);
+          books = this.changeBookShelf(books);
+          this.setState(() => {
+            return { books };
+          });
+        }
+      });
+    } else {
+      this.setState({ books: [], query: "" });
+    }
+  };
+
+  addBook = (book, shelf) => {
+    this.props.onChangeShelf(book, shelf);
+  };
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link className="close-search" to="/">Close</Link>
+          <Link to="/" className="close-search">
+            Close
+          </Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" onChange={this.onSearch} />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={this.handleChange}
+            />
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.searchResults && this.state.searchResults.map(book => (
-              <li key={book.id}>
-                <Book book={book} onChangeShelf={this.props.onChangeShelf} />
-              </li>
-            ))}
+            {this.state.query.length > 0 &&
+              this.state.books.map((book, index) => (
+                <Book
+                  book={book}
+                  key={index}
+                  onUpdate={(shelf) => {
+                    this.addBook(book, shelf);
+                  }}
+                />
+              ))}
           </ol>
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default SearchBook;
